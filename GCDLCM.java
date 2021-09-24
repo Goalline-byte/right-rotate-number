df9e7883684fca237756a740006791e2b597dbd6
rajputaradhna <aradhnatnsingh01@gmail.com>
2021-09-24 22:54:33 +0530

    easy concept

---
+++ GCDLCM.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class GCDLCM{
+
+public static void main(String[] args) {
+
+    Scanner scn = new Scanner(System.in);
+    int n1 = scn.nextInt();
+    int n2 = scn.nextInt();
+
+    int a1 = n1;
+    int a2 = n2;
+
+    while(n1 % n2 != 0){
+        int rem = n1 % n2;
+        n1 = n2;
+        n2 = rem;
+    }
+    int gcd = n2;
+    int lcm = (a1 * a2) / gcd;
+
+    System.out.println(gcd);
+    System.out.println(lcm);
+
+}
+
+    
+}
